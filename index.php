0659287cca73b8521734e4eb134e69d06f26f8e6
jibon <jibonkhan53296@gmail.com>
2023-05-24 15:46:29 +0600

    this is demo file

--- index.php
+++ index.php
@@ -18,6 +18,10 @@
         <li><a href="#">About Us</a></li>
         <li><a href="#">Gallary</a></li>
         <li><a href="#">Contact Us</a></li>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About Us</a></li>
+        <li><a href="#">Gallary</a></li>
+        <li><a href="#">Contact Us</a></li>
     </ul>
 </menu>
 <section id="all">
