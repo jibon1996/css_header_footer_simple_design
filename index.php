d1ecdea9046c0de8468b1c68fe4ec73eac64dec6
jibon <jibonkhan53296@gmail.com>
2023-05-24 15:26:16 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,44 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Document</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+<header>
+    <h1>jibon khan</h1>
+</deader>
+<menu>
+    <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About Us</a></li>
+        <li><a href="#">Gallary</a></li>
+        <li><a href="#">Contact Us</a></li>
+    </ul>
+</menu>
+<section id="all">
+    <div class="content">
+       <h1>Our Services</h1>
+       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, nostrum tempore. Nulla ut distinctio eius eaque ab veniam porro cumque id qui neque dicta enim nostrum velit animi commodi tenetur aliquam nihil, repellendus inventore dolorem facilis numquam. Reprehenderit similique blanditiis veniam fuga modi nihil cupiditate repellendus et adipisci. Nesciunt, consequuntur! Ducimus hic, aut nesciunt a est molestias facere in accusantium maxime? Facere magnam, reprehenderit accusantium dolor molestias totam sunt veniam eos officia optio possimus veritatis. Et tenetur, cum, suscipit fugiat, quidem distinctio modi in expedita nesciunt exercitationem corporis voluptatum? Perspiciatis, amet error eum perferendis quibusdam quas pariatur aut quasi a.</p>
+
+     
+    </div>
+    <div class="slider">
+    <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About Us</a></li>
+        <li><a href="#">Gallary</a></li>
+        <li><a href="#">Contact Us</a></li>
+    </ul>
+    </div>
+</section>
+
+<footer>
+    <p>jibonkhan Copyright &copy; 2023</p>
+</footer>
+</body>
+</html>
